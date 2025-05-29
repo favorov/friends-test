7ed48b1959fc3d951963f470142439abf567d043
Alexander Favorov <favorov@sensi.org>
2025-05-28 21:31:48 -0400

    Added underscores to see two tests

--- R/mixture-model-test.r
+++ R/mixture-model-test.r
@@ -1,32 +1,32 @@
-n   <- 400     
+n_   <- 400     
 # число строк 
-k   <- 50       
+k_   <- 50       
 # число столбцов 
-n0  <- 10       
+n0_  <- 10       
 # сколько строк «портим» 
-p   <- 0.1      
+p_   <- 0.1      
 # вероятность замены 
-rate_exp    <- 1/2    
+rate_exp_    <- 1/2    
 # rate для Exp 
-lambda_pois <- 100   
+lambda_pois_ <- 100   
 # λ для Pois  
 #-------------------------  # 
 #stat <- c() # 
 #false <- c() # 
 for (i in 1:1){    
   # 1) Генерация матрицы 
-  M <- matrix(rexp(n * k, rate = rate_exp), nrow = n, ncol = k)  
+  M_ <- matrix(rexp(n_ * k_, rate = rate_exp_), nrow = n_, ncol = k_)  
   # 2) Отметка строк и маскирование 
-  selected_rows <- sort(sample.int(n, size = n0))
-  mask <- matrix(FALSE, nrow = n, ncol = k) 
-  mask[selected_rows, ] <- 
-    matrix(runif(n0 * k) < p, nrow = n0, ncol = k, byrow=TRUE)  
+  selected_rows_ <- sort(sample.int(n_, size = n0_))
+  mask_ <- matrix(FALSE, nrow = n_, ncol = k_) 
+  mask_[selected_rows_, ] <- 
+    matrix(runif(n0_ * k_) < p_, nrow = n0_, ncol = k_, byrow=TRUE)  
   # 3) Замена отмеченных ячеек на Poisson 
-  M[mask] <- rpois(sum(mask), lambda = lambda_pois)  
+  M_[mask_] <- rpois(sum(mask_), lambda = lambda_pois_)  
   # Задаём имена столбцов: "col_1", "col_2", …, "col_k" 
-  colnames(M) <- paste0("col_", seq_len(k))  
+  colnames(M_) <- paste0("col_", seq_len(k_))  
   # Задаём имена строк: "row_1", "row_2", …, "row_n" 
-  rownames(M) <- paste0("row_", seq_len(n))
+  rownames(M_) <- paste0("row_", seq_len(n_))
 }
 
 
