5acc5586d87257e06cb7d147f3dd7f7c11d370bc
Alexander Favorov <favorov@sensi.org>
2025-07-23 11:50:45 -0400

    linting

--- R/plot-mixture-model.R
+++ R/plot-mixture-model.R
@@ -1,102 +1,115 @@
 
-#nM<-M/rowSums(M)
-Morig<-M
-scale<-1/2
-scaleMask<-mask
-scaleMask[selected_rows,]=!mask[selected_rows]
-M[scaleMask]=M[scaleMask]*scale
+#if we want to normalize the matrix, nM<-M/rowSums(M)
+Morig <- M
+scale <- 1/2
+scaleMask <- mask
+scaleMask[selected_rows, ] = !mask[selected_rows]
+M[scaleMask] = M[scaleMask] * scale
 
 
-hM<-ComplexHeatmap::Heatmap(M,
+hM <- ComplexHeatmap::Heatmap(M,
   name = "Mixture test", # Name of the heatmap legend
   cluster_rows = FALSE,
   cluster_columns = FALSE,
   show_row_names = FALSE,
   show_column_names = FALSE,
   heatmap_legend_param = list(title = "Value"), # Customize legend title
-  col = colorRamp2::colorRamp2(c(0, 0, max(M)),
-  c("white", "white", "red")), # Optional: custom color scheme
+  col = colorRamp2::colorRamp2(
+    c(0, 0, max(M)),
+    c("white", "white", "red")
+  ), # Optional: custom color scheme
   column_title = "mixture"
 )
 
-falsecolor<-"gray98"
+falsecolor <- "gray98"
 
-friends<-friends.test::friends.test.bic(M,prior.to.have.friends = 0.001)
+friends <- friends.test::friends.test.bic(M, prior.to.have.friends = 0.001)
 
 
-hmask<-ComplexHeatmap::Heatmap(mask,
-             name = "mask", # Name of the heatmap legend
-             cluster_rows = FALSE,
-             cluster_columns = FALSE,
-             show_row_names = FALSE,
-             show_column_names = FALSE,
-             col = colorRamp2::colorRamp2(c(0, 0, 1),
-                                          c(falsecolor, falsecolor, "red")), # Optional: custom color scheme
-             column_title = "mask",
-             show_heatmap_legend = FALSE
+hmask <- ComplexHeatmap::Heatmap(
+  mask,
+  name = "mask", # Name of the heatmap legend
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+  show_row_names = FALSE,
+  show_column_names = FALSE,
+  col = colorRamp2::colorRamp2(
+    c(0, 0, 1),
+    c(falsecolor, falsecolor, "red")
+  ), # Optional: custom color scheme
+  column_title = "mask",
+  show_heatmap_legend = FALSE
 )
 
 
-friends.mat<-matrix(0,nrow=nrow(M),ncol=ncol(M))
-rownames(friends.mat)<-rownames(M)
-colnames(friends.mat)<-colnames(M)
+friends.mat <- matrix(0, nrow = nrow(M), ncol = ncol(M))
+rownames(friends.mat) <- rownames(M)
+colnames(friends.mat) <- colnames(M)
 
-for(r in seq(nrow(friends))){
-  friends.mat[friends[r,"marker"],friends[r,"friend"]]=1
+for (r in seq(nrow(friends))) {
+  friends.mat[friends[r, "marker"], friends[r, "friend"]] = 1
 }
 
-hfb<-ComplexHeatmap::Heatmap(friends.mat,
-            name = "Friends", # Name of the heatmap legend
-            cluster_rows = FALSE,
-            cluster_columns = FALSE,
-            show_row_names = FALSE,
-            show_column_names = FALSE,
-            col = colorRamp2::colorRamp2(c(0, 0, 1),
-                          c(falsecolor, falsecolor, "orange")), # Optional: custom color scheme
-            column_title = "friends.test.bic",
-            show_heatmap_legend = FALSE
+hfb <- ComplexHeatmap::Heatmap(
+  friends.mat,
+  name = "Friends", # Name of the heatmap legend
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+  show_row_names = FALSE,
+  show_column_names = FALSE,
+  col = colorRamp2::colorRamp2(
+    c(0, 0, 1),
+    c(falsecolor, falsecolor, "orange")
+  ), # Optional: custom color scheme
+  column_title = "friends.test.bic",
+  show_heatmap_legend = FALSE
 )
 
 pdf("mixture_and_friends_bic.pdf")
-plot(hM+hmask+hfb)
+plot(hM + hmask + hfb)
 #dev.off()
 
-friends<-friends.test::friends.test(M)
+friends <- friends.test::friends.test(M)
 
 
-hmask<-ComplexHeatmap::Heatmap(mask,
-               name = "mask", # Name of the heatmap legend
-               cluster_rows = FALSE,
-               cluster_columns = FALSE,
-               show_row_names = FALSE,
-               show_column_names = FALSE,
-               col = colorRamp2::colorRamp2(c(0, 0, 1),
-                                            c(falsecolor, falsecolor, "red")), # Optional: custom color scheme
-               column_title = "mask",
-               show_heatmap_legend = FALSE
+hmask <- ComplexHeatmap::Heatmap(
+  mask,
+  name = "mask", # Name of the heatmap legend
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+  show_row_names = FALSE,
+  show_column_names = FALSE,
+  col = colorRamp2::colorRamp2(
+    c(0, 0, 1),
+    c(falsecolor, falsecolor, "red")
+  ), # Optional: custom color scheme
+  column_title = "mask",
+  show_heatmap_legend = FALSE
 )
 
 
-friends.mat<-matrix(0,nrow=nrow(M),ncol=ncol(M))
-rownames(friends.mat)<-rownames(M)
-colnames(friends.mat)<-colnames(M)
+friends.mat <- matrix(0, nrow = nrow(M), ncol = ncol(M))
+rownames(friends.mat) <- rownames(M)
+colnames(friends.mat) <- colnames(M)
 
-for(r in seq(nrow(friends))){
-  friends.mat[friends[r,"marker"],friends[r,"friend"]]=1
+for (r in seq(nrow(friends))) {
+  friends.mat[friends[r, "marker"], friends[r, "friend"]] = 1
 }
 
-hfb<-ComplexHeatmap::Heatmap(friends.mat,
-             name = "Friends", # Name of the heatmap legend
-             cluster_rows = FALSE,
-             cluster_columns = FALSE,
-             show_row_names = FALSE,
-             show_column_names = FALSE,
-             col = colorRamp2::colorRamp2(c(0, 0, 1),
-                                          c(falsecolor, falsecolor, "orange")), # Optional: custom color scheme
-             column_title = "friends.test",
-             show_heatmap_legend = FALSE
+hfb <- ComplexHeatmap::Heatmap(
+  friends.mat,
+  name = "Friends", # Name of the heatmap legend
+  cluster_rows = FALSE,
+  cluster_columns = FALSE,
+  show_row_names = FALSE,
+  show_column_names = FALSE,
+  col = colorRamp2::colorRamp2(
+    c(0, 0, 1),
+    c(falsecolor, falsecolor, "orange")
+  ), # Optional: custom color scheme
+  column_title = "friends.test",
+  show_heatmap_legend = FALSE
 )
 
-plot(hM+hmask+hfb)
+plot(hM + hmask + hfb)
 dev.off()
-
