e5518fa13337febdd13b0b456e2a65b5f97ea3af
Sasha Favorov <favorov@sensi.org>
2026-01-15 00:41:36 -0500

    nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,5 @@
+vim.opt.tabstop = 4       -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true  -- Pressing the TAB key will insert spaces
+vim.opt.softtabstop = 4   -- Number of spaces inserted for a TAB key press
+vim.opt.shiftwidth = 4    -- Number of spaces inserted when indenting
+w
