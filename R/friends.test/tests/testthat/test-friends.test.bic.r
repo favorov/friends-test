b4d40e8ef1dda3a9183a402e050a896046b36e29
Sasha Favorov <favorov@sensi.org>
2026-01-26 19:04:57 -0500

    coll -> col

--- R/friends.test/tests/testthat/test-friends.test.bic.r
+++ R/friends.test/tests/testthat/test-friends.test.bic.r
@@ -1,12 +1,12 @@
 test_that("no errors in simplest case", {
     mat <- diag(nrow = 5, ncol = 5)
     rownames(mat) <- paste0("row", 1:5)
-    colnames(mat) <- paste0("coll", 1:5)
+    colnames(mat) <- paste0("col", 1:5)
     expect_no_error(friends.test.bic(mat, .5))
 })
 
 test_that("best friend is determined correctly", {
-    text <- "    coll1     coll2     coll3      coll4     coll5
+    text <- "    col1     col2     coll      col4     col5
             row1 0.1765568 0.7176185 0.2121425 0.01339033 0.5995658
             row2 0.6870228 0.9919061 0.6516738 0.38238796 0.4935413
             row3 0.3841037 0.3800352 0.1255551 0.86969085 0.1862176
@@ -15,24 +15,23 @@ test_that("best friend is determined correctly", {
     attention <- as.matrix(read.table(text = text, header = TRUE))
 
     friends <- friends.test.bic(attention, .25, max.friends.n = 1)
-    expect_equivalent(dim(friends), dim(attention))
-    expected <- sparseMatrix(
-        i = integer(0),
-        j = integer(0),
-        x = numeric(0),
-        repr = "T",
-        dims = dim(attention),
-        dimnames = list(
-            marker = rownames(attention),
-            friend = colnames(attention)
+    expected <- list(
+        row5 = list(
+            col5 = c(marker = 5, friend = 5, rank = 1)
         )
     )
-    expected[5, 5] <- 1
     expect_equivalent(friends, expected)
 
     friends <- friends.test.bic(attention, .5, max.friends.n = 1)
-    expect_equivalent(dim(friends), dim(attention))
-    expected[3, 4] <- 1 # we expect 2 now
+    #we expct two friend now
+    expected <- list(
+        row3 = list(
+            col4 = c(marker = 3, friend = 4, rank = 1)
+        ),
+        row5 = list(
+            col5 = c(marker = 5, friend = 5, rank = 1)
+        )
+    )
     expect_equivalent(friends, expected)
 })
 
@@ -40,24 +39,45 @@ test_that("best friend is determined correctly", {
 # we will use a rectangular matrix for this test (ncolls << nrows)
 set.seed(1) # actually, it works with like 9/10 of seeds
 nrows <- 100
-ncolls <- 10
-almost_diagon_mat <- matrix(1 + 9 * runif(nrows * ncolls), nrow = nrows)
-almost_diagon_mat[1:ncolls, ] <- runif(ncolls * ncolls)
+ncols <- 10
+almost_diagon_mat <- matrix(1 + 9 * runif(nrows * ncols), nrow = nrows)
+almost_diagon_mat[1:ncolls, ] <- runif(ncolls * ncols)
 diag(almost_diagon_mat) <- 19
 rownames(almost_diagon_mat) <- paste0("row", 1:nrows)
-colnames(almost_diagon_mat) <- paste0("coll", 1:ncolls)
-probe <- sparseMatrix(
-    i = integer(0),
-    j = integer(0),
-    x = numeric(0),
-    repr = "T",
-    dims = dim(almost_diagon_mat),
-    dimnames = list(
-        marker = rownames(almost_diagon_mat),
-        friend = colnames(almost_diagon_mat)
+colnames(almost_diagon_mat) <- paste0("col", 1:ncols)
+
+expected <- list(
+    row1 = list(
+        col1 = c(marker = 1, friend = 1, rank = 1)
+    ),
+    row2 = list(
+        col2 = c(marker = 2, friend = 2, rank = 1)
+    ),
+    row3 = list(
+        col3 = c(marker = 3, friend = 3, rank = 1)
+    ),
+    row4 = list(
+        col4 = c(marker = 4, friend = 4, rank = 1)
+    ),
+    row5 = list(
+        col5 = c(marker = 5, friend = 5, rank = 1)
+    ),
+    row6 = list(
+        col6 = c(marker = 6, friend = 6, rank = 1)
+    ),
+    row7 = list(
+        col7 = c(marker = 7, friend = 7, rank = 1)
+    ),
+    row8 = list(
+        col8 = c(marker = 8, friend = 8, rank = 1)
+    ),
+    row9 = list(
+        col9 = c(marker = 9, friend = 9, rank = 1)
+    ),
+    row10 = list(
+        col10 = c(marker = 10, friend = 10, rank = 1)
     )
 )
-probe[cbind(1:10, 1:10)] <- 1
 
 
 test_that("passes non-diagonal diagonal test with low prior to have friend", {
@@ -67,6 +87,8 @@ test_that("passes non-diagonal diagonal test with low prior to have friend", {
 
 test_that("passes non-diagonal diagonal test with high prior to have friend", {
     res <- friends.test.bic(almost_diagon_mat, .5)
-    expect_equivalent(((res != 0) & (probe != 0)), (probe != 0))
-    # we test that probe is contained in res
+    resflat <- purrr::flatten(res)
+    expflat <- purrr::flatten(exp)
+    expect_equivalent(setdiff(resflat, expflat), list())
+    # we test that result is contained in res
 })
