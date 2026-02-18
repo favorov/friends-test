050d63b506fd5aae83ae5de30fe1defb6d37d4e3
Sasha Favorov <favorov@sensi.org>
2026-02-18 16:52:13 -0500

    added the markers as of CoGAPS to the package data

---
+++ R/friends.test/R/cogaps_loadings_example_markers.r
@@ -0,0 +1,5 @@
+#' The CoGAPS::patterMarkers result for the example of
+#' loadings of genes in CoGAPS - identified patterns [cogaps_loadings_example].
+#'
+#' @source <https://zenodo.org/records/7709664/files/cogapsresult.Rds>
+"cogaps_loadings_example_markers"
