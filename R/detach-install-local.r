a90540a7ba993ae3144d4eac4e97e2ee1d5d8da5
Sasha Favorov <favorov@sensi.org>
2026-02-18 18:28:11 -0500

    lint

--- R/detach-install-local.r
+++ R/detach-install-local.r
@@ -1,5 +1,5 @@
 #run from friends.test 
-detach("package:friends.test", unload=TRUE, character.only= TRUE)
+detach("package:friends.test", unload = TRUE, character.only = TRUE)
 devtools::document()
-devtools::install_local(force=TRUE)
+devtools::install_local(force = TRUE)
 library(friends.test)
