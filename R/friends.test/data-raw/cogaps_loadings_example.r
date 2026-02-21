7d08a9bc9bc6383b0a8d59b6acf035605d5a776a
Sasha Favorov <favorov@sensi.org>
2026-02-21 01:25:07 -0500

    do not save copaps markers on this branch

--- R/friends.test/data-raw/cogaps_loadings_example.r
+++ R/friends.test/data-raw/cogaps_loadings_example.r
@@ -6,7 +6,7 @@ suppressWarnings({
         "https://zenodo.org/records/7709664/files/cogapsresult.Rds"
     )))
     cogaps_loadings_example <- cg@featureLoadings
-    cogaps_loadings_example_markers <- patternMarkers(cg)
+    # unusefull currently cogaps_loadings_example_markers <- patternMarkers(cg)
 })
 usethis::use_data(cogaps_loadings_example, overwrite = TRUE)
-usethis::use_data(cogaps_loadings_example_markers, overwrite = TRUE)
+# unusefull currently usethis::use_data(cogaps_loadings_example_markers, overwrite = TRUE)
