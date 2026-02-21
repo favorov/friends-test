a5d916e713104575a6448a45248efd7fb94283ff
Sasha Favorov <favorov@sensi.org>
2026-02-21 14:46:44 -0500

    reaming copaps example

--- R/friends.test/R/cogaps_loadings_example.r
+++
@@ -1,4 +0,0 @@
-#' An exaple of loadings of genes in CoGAPS - identified patterns.
-#'
-#' @source <https://zenodo.org/records/7709664/files/cogapsresult.Rds>
-"cogaps_loadings_example"
--- R/friends.test/R/cogaps_loadings_example_markers.r
+++
@@ -1,5 +0,0 @@
-#' The CoGAPS::patterMarkers result for the example of
-#' loadings of genes in CoGAPS - identified patterns [cogaps_loadings_example].
-#'
-#' @source <https://zenodo.org/records/7709664/files/cogapsresult.Rds>
-"cogaps_loadings_example_markers"
