de72fea3ed073f9fa524292635b9a6d7911a6d83
Sasha Favorov <favorov@sensi.org>
2026-03-16 21:51:00 -0400

    fgsea results for cogaps loadings are in the saved data

--- R/friends.test/data-raw/cogaps_example.r
+++ R/friends.test/data-raw/cogaps_example.r
@@ -1,5 +1,8 @@
 library(usethis)
 library(CoGAPS)
+library('fgsea')
+library('msigdbr')
+
 options(timeout = 3000)
 friends.test.cogaps.example <- list()
 suppressWarnings({
@@ -13,5 +16,51 @@ suppressWarnings({
         patternMarkers(cg, threshold = "cut")
     friends.test.cogaps.example$markers_all <-
         patternMarkers(cg, threshold = "all")
+
+
+    msigdbr_H_df <- msigdbr::msigdbr(species = "human", collection = "H")
+    collections_H <- split(
+        msigdbr_H_df$gene_symbol,
+        msigdbr_H_df$gs_name
+    )
+    collections_H <- split(
+        msigdbr_H_df$gene_symbol,
+        msigdbr_H_df$gs_name
+    )
+
+    friends.test.cogaps.example$hallmarks <-
+        purrr::map(colnames(friends.test.cogaps.example$loadings), \(pat) {
+            fgsea::fgsea(
+                pathways = collections_H,
+                stats = friends.test.cogaps.example$loadings[, pat]
+            ) |>
+                dplyr::filter(padj < 0.05) |>
+                dplyr::select(pathway, padj) |>
+                dplyr::arrange(padj)
+        })
+
+
+    msigdbr_C4_3CA_df <-
+        msigdbr::msigdbr(
+            species = "human",
+            collection = "C4",
+            subcollection = "3CA"
+        )
+    collections_C4_3CA <- split(
+        msigdbr_C4_3CA_df$gene_symbol,
+        msigdbr_C4_3CA_df$gs_name
+    )
+
+    friends.test.cogaps.example$C4_3CA <-
+        purrr::map(colnames(friends.test.cogaps.example$loadings), \(pat) {
+            fgsea::fgsea(
+                pathways = collections_C4_3CA,
+                stats = friends.test.cogaps.example$loadings[, pat]
+            ) |>
+                dplyr::filter(padj < 0.05) |>
+                dplyr::select(pathway, padj) |>
+                dplyr::arrange(padj)
+        })
 })
+
 usethis::use_data(friends.test.cogaps.example, overwrite = TRUE)
