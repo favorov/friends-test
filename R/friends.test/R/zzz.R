4e9b9a283dbff0121576be5e3af8bcf38bd0925c
Sasha Favorov <favorov@sensi.org>
2026-06-02 03:11:59 -0400

    added .

--- R/friends.test/R/zzz.R
+++ R/friends.test/R/zzz.R
@@ -3,7 +3,7 @@
     version <- packageDescription("friends.test", fields = "Version")
     yiddish <- paste0(
         "\u05E2\u05E8\u05E9\u05D8\u05E2\u05E8",
-        " \u05D6\u05D5\u05DE\u05E2\u05E8\u05BE\u05D8\u05D0\u05B8\u05D2"
+        " \u05D6\u05D5\u05DE\u05E2\u05E8\u05BE\u05D8\u05D0\u05B8\u05D2."
     )
     yiddish_display <- if (Sys.getenv("TERM_PROGRAM") == "vscode") {
         intToUtf8(rev(utf8ToInt(yiddish)))
